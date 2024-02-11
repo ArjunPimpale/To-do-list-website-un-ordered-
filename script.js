aa8cd66d4f75b3f077451544493cdaf55032fa93
ArjunPimpale <pimpalearjun10@gmail.com>
2024-02-11 13:12:18 +0530

    Added input text

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ let rev = document.querySelector(".container");
 let ref = document.querySelector(".container").querySelector(".tasklist");
 
 function getdata() {
-    // Iterate through localStorage keys
+   
     for (let i = 0; i < localStorage.length; i++) {
-        let key = localStorage.key(i); // Get the key at index i
-        let value = localStorage.getItem(key); // Get the value for the key
-        ref.insertAdjacentHTML("beforeend", value); // Insert the value into the DOM
+        
+        let key = localStorage.key(i);
+        let value = localStorage.getItem(key); 
+        if(value == null)
+            continue;
+        ref.insertAdjacentHTML("beforeend", value); 
     }
 }
 
@@ -24,15 +27,17 @@ function taskdeletor(x) {
    localStorage.removeItem(`task${x}`);
    ref.querySelector(`#task${x}`).remove();
 }
-function inserttask(){
+function inserttask(event){
+    event.preventDefault();
+
      let i = localStorage.length;
-     let a = prompt("Enter the task");
+     let a = document.getElementById("Enternotes").value;
      localStorage.setItem(`task${i}`, `<div class = "task" id = "task${i}"><p>${a}<\p> <button class = "delete" onclick="taskdeletor(${i})" >delete</button></div>`)
      ref.insertAdjacentHTML("beforeend",`${localStorage.getItem(`task${i}`)}`)
 
  }
-
- rev.querySelector(".Add").addEventListener("click",inserttask);
+ const form = document.getElementById("submitform");
+ form.addEventListener('submit', inserttask);
 
 
  rev.querySelector(".clear").addEventListener("click",()=>{
